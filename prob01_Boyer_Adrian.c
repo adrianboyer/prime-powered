a44373133d42bb9d1e91a9df97318e91a278621d
Adrian Boyer <adrian.boyer-paulet@yale.edu>
2026-09-18 00:06:50 -0400

    Add files via upload

---
+++ prob01_Boyer_Adrian.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Adrian Boyer-Paulet\n");
+	printf("I am taking this course to learn more about how to program in C and to know how to code in general, not to mention improving my problem-solving skills!\n");
+	printf("\n");
+	printf("I have read the course policy on generative AI, the collaboration policy, and the statement on academic integrity. I understand that by adhering to them, I will help to create an equitable learning environment for everyone.\n");
+
+	return 0;
+}
